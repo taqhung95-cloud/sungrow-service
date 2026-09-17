c54f58be629e3a3799cffc1e4d4f05ca104532cb
taqhung95-cloud <taqhung95-cloud@users.noreply.github.com>
2026-09-17 17:50:05 +0700

    Configure Apps Script endpoint

--- docs/config.js
+++ docs/config.js
@@ -1,5 +1,5 @@
 window.SUNGROW_CONFIG = {
-  appsScriptUrl: 'PASTE_APPS_SCRIPT_EXEC_URL_HERE',
+  appsScriptUrl: 'https://script.google.com/macros/s/AKfycby1AAK7NUgCfANnBy9JdRod4Hdf45e0Oa2SIzA_DA7DwnWQkS73XdkkFfgjUNdFb9D8/exec',
   googleClientId: 'PASTE_GOOGLE_WEB_CLIENT_ID_HERE',
   defaultPeriod: '2026-09'
 };
