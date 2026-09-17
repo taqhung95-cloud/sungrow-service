d2e09b548155d5e0fcbc66b8aacd5f5ab20ba03a
taqhung95-cloud <taqhung95-cloud@users.noreply.github.com>
2026-09-17 18:06:00 +0700

    Configure Google sign-in client

--- docs/config.js
+++ docs/config.js
@@ -1,5 +1,5 @@
 window.SUNGROW_CONFIG = {
   appsScriptUrl: 'https://script.google.com/macros/s/AKfycby1AAK7NUgCfANnBy9JdRod4Hdf45e0Oa2SIzA_DA7DwnWQkS73XdkkFfgjUNdFb9D8/exec',
-  googleClientId: 'PASTE_GOOGLE_WEB_CLIENT_ID_HERE',
+  googleClientId: '1057611730150-6ds8o36jv1haln4h6tcl1gilh31o7hqn.apps.googleusercontent.com',
   defaultPeriod: '2026-09'
 };
